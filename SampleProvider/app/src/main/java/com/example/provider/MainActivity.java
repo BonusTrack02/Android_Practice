77739b2e20d8058a82a00e8149f7055de70cac4e
BonusTrackKR <wlsgud321@gmail.com>
2021-07-05 23:23:39 +0900

    p.566 ContentProvider를 사용해 Database CRUD 실행

--- SampleProvider/app/src/main/java/com/example/provider/MainActivity.java
+++ SampleProvider/app/src/main/java/com/example/provider/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.provider;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                insertPerson();
             }
         });
 
@@ -28,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                queryPerson();
             }
         });
 
@@ -35,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                updatePerson();
             }
         });
 
@@ -42,8 +48,78 @@ public class MainActivity extends AppCompatActivity {
         button4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                deletePerson();
             }
         });
+    }
+
+    public void insertPerson() {
+        println("insertPerson 호출됨.");
+
+        String uriString = "content://com.example.provider/person";
+        Uri uri = new Uri.Builder().build().parse(uriString);
+
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+        String[] columns = cursor.getColumnNames();
+        println("columns count -> " + columns.length);
+
+        for (int i=0; i<columns.length; i++) {
+            println("#" + i + " : " + columns[i]);
+        }
+
+        ContentValues values = new ContentValues();
+        values.put("name", "John");
+        values.put("age", 20);
+        values.put("mobile", "010-1000-1000");
+
+        uri = getContentResolver().insert(uri, values);
+        println("insert 결과 -> " + uri.toString());
+    }
+
+    public void queryPerson() {
+        try {
+            String uriString = "content://com.example.provider/person";
+            Uri uri = new Uri.Builder().build().parse(uriString);
+
+            String[] columns = new String[] {"name", "age", "mobile"};
+            Cursor cursor = getContentResolver().query(uri, columns, null, null, "name ASC");
+            println("query 결과 : " + cursor.getCount());
+
+            int index = 0;
+            while (cursor.moveToNext()) {
+                String name = cursor.getString(cursor.getColumnIndex(columns[0]));
+                int age = cursor.getInt(cursor.getColumnIndex(columns[1]));
+                String mobile = cursor.getString(cursor.getColumnIndex(columns[2]));
+
+                println("#" + index + " -> " + name + ", " + age + ", " + mobile);
+                index += 1;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updatePerson() {
+        String uriString = "content://com.example.provider/person";
+        Uri uri = new Uri.Builder().build().parse(uriString);
+
+        String selection = "mobile = ?";
+        String[] selectionArgs = new String[] {"010-1000-1000"};
+        ContentValues updateValue = new ContentValues();
+        updateValue.put("mobile", "010-2000-2000");
+        int count = getContentResolver().update(uri, updateValue, selection, selectionArgs);
+        println("update 결과 : " + count);
+    }
+
+    public void deletePerson() {
+        String uriString = "content://com.example.provider/person";
+        Uri uri = new Uri.Builder().build().parse(uriString);
+
+        String selection = "name = ?";
+        String[] selectionArgs = new String[] {"John"};
+
+        int count = getContentResolver().delete(uri, selection, selectionArgs);
+        println("delete 결과 : " + count);
     }
 
     public void println(String data) {
