0ca6eccde6fe0c2eda99ca03cf0807dc236cea3a
BonusTrackKR <wlsgud321@gmail.com>
2021-07-10 02:31:27 +0900

    p.592 colors.xml에 color name을 지정하여 LinearGradient를 사용

---
+++ SampleCustomViewDrawable/app/src/main/java/com/example/graphics/custom/drawable/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.graphics.custom.drawable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        CustomViewDrawable view = new CustomViewDrawable(this);
+        setContentView(view);
+    }
+}
