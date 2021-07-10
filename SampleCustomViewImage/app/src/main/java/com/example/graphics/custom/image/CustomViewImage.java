c07f5196a20f076045d9721e6d08396cc0bb2ef2
BonusTrackKR <wlsgud321@gmail.com>
2021-07-10 20:57:29 +0900

    p.599 상하 대칭 이미지를 만들고 이미지를 확대한 후 블러 효과 또한 적용 실습

--- SampleCustomViewImage/app/src/main/java/com/example/graphics/custom/image/CustomViewImage.java
+++ SampleCustomViewImage/app/src/main/java/com/example/graphics/custom/image/CustomViewImage.java
@@ -2,8 +2,11 @@ package com.example.graphics.custom.image;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
@@ -45,6 +48,23 @@ public class CustomViewImage extends View {
         cacheCanvas.drawColor(Color.WHITE);
         mPaint.setColor(Color.RED);
         cacheCanvas.drawRect(100, 100, 200, 200, mPaint);
+
+        Bitmap srcImg = BitmapFactory.decodeResource(getResources(), R.drawable.waterdrop);
+        cacheCanvas.drawBitmap(srcImg, 30, 30, mPaint);
+
+        Matrix horInverseMatrix = new Matrix();
+        horInverseMatrix.setScale(-1, 1);
+        Bitmap horInverseImg = Bitmap.createBitmap(srcImg, 0, 0, srcImg.getWidth(), srcImg.getHeight(), horInverseMatrix, false);
+        cacheCanvas.drawBitmap(horInverseImg, 30, 130, mPaint);
+
+        Matrix verInverseMatrix = new Matrix();
+        verInverseMatrix.setScale(1, -1);
+        Bitmap verInverseImg = Bitmap.createBitmap(srcImg, 0, 0, srcImg.getWidth(), srcImg.getHeight(), verInverseMatrix, false);
+        cacheCanvas.drawBitmap(verInverseImg, 30, 230, mPaint);
+
+        mPaint.setMaskFilter(new BlurMaskFilter(10, BlurMaskFilter.Blur.NORMAL));
+        Bitmap scaledImg = Bitmap.createScaledBitmap(srcImg, srcImg.getWidth()*3, srcImg.getHeight()*3, false);
+        cacheCanvas.drawBitmap(scaledImg, 30, 300, mPaint);
     }
 
     @Override
