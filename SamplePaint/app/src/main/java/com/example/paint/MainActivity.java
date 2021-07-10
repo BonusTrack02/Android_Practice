d3ede9e4d8a0b5c04cb581a6e5a068c64dc813fe
BonusTrackKR <wlsgud321@gmail.com>
2021-07-11 02:23:59 +0900

    p.607 BestPaintBoard class를 통해 택배 수령 시 사인을 받는 기능 구현// 기존PaintBoard class보다 부드러운 곡선 처리가 가능

--- SamplePaint/app/src/main/java/com/example/paint/MainActivity.java
+++ SamplePaint/app/src/main/java/com/example/paint/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        PaintBoard view = new PaintBoard(this);
+        BestPaintBoard view = new BestPaintBoard(this);
         setContentView(view);
     }
 }
