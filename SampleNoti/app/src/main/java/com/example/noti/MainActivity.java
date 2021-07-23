75937f5264beefbc3d3ecebc47f53f1442557c65
BonusTrackKR <wlsgud321@gmail.com>
2021-07-23 23:33:46 +0900

    p.710 두 번째 버튼을 추가하고 클릭 시 알림을 보냄. 보낸 알림은 누르면 사라지며 MainActivity 화면을 출력

--- SampleNoti/app/src/main/java/com/example/noti/MainActivity.java
+++ SampleNoti/app/src/main/java/com/example/noti/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.core.app.NotificationCompat;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -17,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
     private static String CHANNEL_ID = "channel1";
     private static String CHANNEL_NAME = "Channel1";
 
+    private static String CHANNEL_ID2 = "channel2";
+    private static String CHANNEL_NAME2 = "Channel2";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +32,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 showNoti1();
+            }
+        });
+
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showNoti2();
             }
         });
     }
@@ -51,5 +64,34 @@ public class MainActivity extends AppCompatActivity {
 
             manager.notify(1, noti);
         }
+    }
+
+    public void showNoti2() {
+        manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        NotificationCompat.Builder builder = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            if (manager.getNotificationChannel(CHANNEL_ID2) == null) {
+                manager.createNotificationChannel(new NotificationChannel(
+                        CHANNEL_ID2, CHANNEL_NAME2, NotificationManager.IMPORTANCE_DEFAULT
+                ));
+
+                builder = new NotificationCompat.Builder(this, CHANNEL_ID2);
+            }
+        } else {
+            builder = new NotificationCompat.Builder(this);
+        }
+
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 101, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentTitle("간단 알림");
+        builder.setContentText("알림 메시지입니다.");
+        builder.setSmallIcon(android.R.drawable.ic_menu_view);
+        builder.setAutoCancel(true);
+        builder.setContentIntent(pendingIntent);
+
+        Notification noti = builder.build();
+
+        manager.notify(2, noti);
     }
 }
